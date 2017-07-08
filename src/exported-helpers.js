3264c2b6147a92201ecdfd14c5f2535fc2cc3b9b
James Calfee <james@jcalfee.info>
2017-07-08 06:45:40 -0500

    Updated eosjs-json and transaction format.

--- src/exported-helpers.js
+++ src/exported-helpers.js
@@ -19,17 +19,21 @@ function createTransaction(api, expireInSeconds = 60, callback) {
   }
   api.getInfo(checkError(callback, info => {
     const chainDate = new Date(info.head_block_time + 'Z')
+    const refBlockNum = (info.head_block_number - 3) & 0xFFFF
+    console.log('refBlockNum', refBlockNum)
     // Back-up 3 blocks to help avoid mini-forks.  The getBlock call does not return
     // its own block id but the next block has 'previous'.
     api.getBlock(info.head_block_num - 2, checkError(callback, block => {
       const headBlockId = block.previous
       const expiration = new Date(chainDate.getTime() + expireInSeconds * 1000)
       const headers = Object.assign({
+        refBlockNum,
+        refBlockPrefix: new Buffer(headBlockId, 'hex').readUInt32LE(4),
         expiration: expiration.toISOString().split('.')[0],
-        tapos: new Buffer(headBlockId, 'hex').readUInt32LE(4),
+        scope: [],
         messages: [],
-        permissions: [],
-        signatures: []
+        signatures: [],
+        authorizations: [],
       })
       callback(null, headers)
     }))
