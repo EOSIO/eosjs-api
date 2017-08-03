bfdefb0e439c378184a2305dd11613de80cc4a4e
James Calfee <james@jcalfee.info>
2017-08-03 08:45:13 -0500

    Fixed import statement, was breaking browser deployments.

--- testnet.js
+++ testnet.js
@@ -4,7 +4,7 @@ module.exports = Testnet
 
 const API_VERSION = 'v1'
 
-Testnet.api = require(`eosjs-json/api/${API_VERSION}`)
+Testnet.api = require('eosjs-json/api/v1')
 
 // Change httpEndpoint to public testnet when available
 const configDefaults = {httpEndpoint: 'http://127.0.0.1:8888'}
