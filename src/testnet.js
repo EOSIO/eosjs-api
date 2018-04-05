bee71017b10b3d4802b079321195c78152970a80
James Calfee <james@jcalfee.info>
2018-04-05 12:17:11 -0500

    Secure endpoint url detection in Eos.Testnet https://github.com/EOSIO/eosjs/issues/42

--- src/testnet.js
+++ src/testnet.js
@@ -1,4 +1,5 @@
 const apiGen = require('./apigen')
+const isBrowser = require('is-browser')
 
 module.exports = Testnet
 
@@ -7,7 +8,11 @@ const API_VERSION = 'v1'
 Testnet.api = require('eosjs-json/api/v1')
 Testnet.schema = require('eosjs-json/schema')
 
-const configDefaults = {httpEndpoint: 'http://t1readonly.eos.io'}
+// Always use SSL unless a browser protocol is 'http'
+const protocol = isBrowser &&
+  location.protocol === 'http:' ? 'http' : 'https'
+
+const configDefaults = {httpEndpoint: `${protocol}://t1readonly.eos.io`}
 
 /**
   @arg {object} config
