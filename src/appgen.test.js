a187bb32e3691f5a45013a76846570c733c35f98
james <james@librem.slim>
2018-07-11 21:34:18 -0500

    Include all unit tests, travis and docker are stable (remove NODE_ENV===development checks)

--- src/appgen.test.js
+++ src/appgen.test.js
@@ -37,79 +37,77 @@ describe('API Generator', function() {
   }
 })
 
-if(process.env['NODE_ENV'] === 'development') {
-  describe('fetch', () => {
-    const definitions = apiVersions.v1
-    const config = {fetchConfiguration: {credentials: 'same-origin'}}
-    const api = apiGen('v1', definitions, config)
+describe('fetch', () => {
+  const definitions = apiVersions.v1
+  const config = {fetchConfiguration: {credentials: 'same-origin'}}
+  const api = apiGen('v1', definitions, config)
 
-    it('getBlock', (done) => {
-      api.getBlock({block_num_or_id: 2}, (err, block) => {
-        if(err) {
-          throw err
-        }
-        assert(block.id, 'block.id')
+  it('getBlock', (done) => {
+    api.getBlock({block_num_or_id: 2}, (err, block) => {
+      if(err) {
+        throw err
+      }
+      assert(block.id, 'block.id')
+      done()
+    })
+  })
+})
+
+it('logging', function (done) {
+  let apiLog
+  const config = {
+    verbose: true,
+    logger: {
+      log: (...args) => {
+        // console.log(...args)
+        apiLog = true
+      },
+      error: (...err) => {
+        // console.log(...err)
+        assert(/callback error/.test(err.join(' ')), 'callback error')
         done()
-      })
-    })
-  })
-
-  it('logging', function (done) {
-    let apiLog
-    const config = {
-      verbose: true,
-      logger: {
-        log: (...args) => {
-          // console.log(...args)
-          apiLog = true
-        },
-        error: (...err) => {
-          // console.log(...err)
-          assert(/callback error/.test(err.join(' ')), 'callback error')
-          done()
-        }
       }
     }
+  }
 
-    const api = apiGen('v1', apiVersions.v1, config)
+  const api = apiGen('v1', apiVersions.v1, config)
 
-    api.getBlock(1, () => {
-      assert(apiLog, 'apiLog')
-      throw 'callback error'
-    })
-  })
-
-  it('api promise error', async function () {
-    let errorLog, apiLog
-    const config = {
-      logger: {
-        error: e => {
-          errorLog = true
-        },
-        log: s => {
-          apiLog = true
-        }
-      }
-    }
-    const api = apiGen('v1', apiVersions.v1, config)
-
-    await api.getBlock(1)
+  api.getBlock(1, () => {
     assert(apiLog, 'apiLog')
-    assert(!errorLog, '!errorLog')
-
-    await api.getBlock('a').catch(e => {
-      assert(errorLog, 'errorLog')
-    })
+    throw 'callback error'
   })
+})
 
-  it('api callback error', function () {
-    let errorLog
-    const config = {logger: {error: e => {
-      errorLog = true
-    }}}
-    const api = apiGen('v1', apiVersions.v1, config)
-    return api.getBlock('a', error => {
-      throw new Error('callback error')
-    })
+it('api promise error', async function () {
+  let errorLog, apiLog
+  const config = {
+    logger: {
+      error: e => {
+        errorLog = true
+      },
+      log: s => {
+        apiLog = true
+      }
+    }
+  }
+  const api = apiGen('v1', apiVersions.v1, config)
+
+  await api.getBlock(1)
+  assert(apiLog, 'apiLog')
+  assert(!errorLog, '!errorLog')
+
+  await api.getBlock('a').catch(e => {
+    assert(errorLog, 'errorLog')
   })
-}
+})
+
+it('api callback error', function () {
+  let errorLog
+  const config = {logger: {error: e => {
+    errorLog = true
+  }}}
+  const api = apiGen('v1', apiVersions.v1, config)
+  return api.getBlock('a', error => {
+    throw new Error('callback error')
+  })
+})
